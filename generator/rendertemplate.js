e360aa089b68a5cfd7547a814aee7b4d3de6cb25
mindthegaptutoring <tbaymath@gmail.com>
2026-03-23 23:05:50 +0700

    Update rendertemplate.js

--- generator/rendertemplate.js
+++ generator/rendertemplate.js
@@ -1,8 +1,17 @@
+// generator/renderTemplate.js
+
 const fs = require("fs");
+const path = require("path");
 const Handlebars = require("handlebars");
 
+// Register basic helpers
+Handlebars.registerHelper("eq", (a, b) => a === b);
+Handlebars.registerHelper("gt", (a, b) => a > b);
+Handlebars.registerHelper("join", (arr, sep) => arr.join(sep));
+
 function renderTemplate(templateName, data) {
-  const template = fs.readFileSync(`./templates/${templateName}`, "utf8");
+  const filePath = path.join(__dirname, "..", "templates", templateName);
+  const template = fs.readFileSync(filePath, "utf8");
   const compiled = Handlebars.compile(template);
   return compiled(data);
 }
